2895a5d7568f3479e771919aece3d4d54274c770
tavodevcode <tavodevcode@gmail.com>
2023-12-18 20:10:10 -0600

    Add dashboard module and component

---
+++ src/app/dashboard/dashboard.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
+
+import { DashboardPageComponent } from './pages/dashboard-page/dashboard-page.component'
+
+import { DashboardRoutingModule } from './dashboard-routing.module'
+import { SharedModule } from '../shared/shared.module'
+
+@NgModule({
+  declarations: [DashboardPageComponent],
+  imports: [CommonModule, DashboardRoutingModule, SharedModule]
+})
+export class DashboardModule {}
