925c1e3cd88bd35c34cd4dfe3f30907d1c5e3377
tavodevcode <tavodevcode@gmail.com>
2023-12-18 20:24:44 -0600

    Add login form to LoginPageComponent

--- src/app/auth/pages/login-page/login.component.ts
+++ src/app/auth/pages/login-page/login.component.ts
@@ -1,8 +1,24 @@
 import { Component } from '@angular/core'
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
 
 @Component({
   selector: 'auth-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginPageComponent {}
+export class LoginPageComponent {
+  constructor(private formBuilder: FormBuilder) {}
+
+  public loginForm: FormGroup = this.formBuilder.group({
+    username: ['', Validators.required],
+    password: ['', Validators.required]
+  })
+
+  get username() {
+    return this.loginForm.get('username')
+  }
+
+  get password() {
+    return this.loginForm.get('password')
+  }
+}
